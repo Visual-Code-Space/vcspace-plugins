f96626383150e3ed37a4d9a519912b73b4536f35
Vivek <itsvks19@gmail.com>
2024-10-27 01:19:11 +0530

    Upload java_snippets.java as a part of Java Snippets plugin

---
+++ plugins/Java Snippets - 1.0.0 (1)/java_snippets.java
@@ -0,0 +1,18 @@
+
+import com.blankj.utilcode.util.ToastUtils;
+import com.teixeira.vcspace.editor.snippet.Snippet;
+import com.teixeira.vcspace.plugins.helper.FileHelper;
+
+import java.util.List;
+
+public void main() {
+  ToastUtils.showShort("Loading Java Snippets");
+    
+  String myPluginPath = PLUGIN_DIR + "/" + manifest.getPackageName();  
+  String snippetsPath = myPluginPath + "/snippets/java.snippets.json";
+  
+  List<Snippet> snippets = helper.loadSnippets(snippetsPath);
+  snippetController.addJavaSnippet(snippets.toArray(new Snippet[0]));
+  
+  ToastUtils.showShort("Java Snippets are loaded successfully 😊");
+}
